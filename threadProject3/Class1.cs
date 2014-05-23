85046c6223a43bdfbd26c0e0f558f30f1cf24e19
jkwendt <jkwendt@noctrl.edu>
2014-05-23 01:06:11 -0500

    leCommit

---
+++ threadProject3/Class1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace threadProject3
+{
+    class Class1
+    {
+        static Object lockObject = new Object();
+        public static void Main()
+        {
+          
+            int [] myArray = {21, 15, 7,37, 23, 90, 51, 84, 43, 61, 60, 27, 76, 24, 86, 3};
+            printArray(myArray);
+            Thread originalQuickSortThread = new Thread(quickSort);
+            System.Console.WriteLine("Sorting Array");
+            originalQuickSortThread.Start(new Range{Array = myArray, Lower = 0, Upper = myArray.Length-1});
+            originalQuickSortThread.Join();
+            lock (lockObject)
+            {
+
+                printArray(myArray);
+            }
+            
+
+        }
+        public static void printArray(int[] arr)
+        {
+            foreach (int a in arr)
+            {
+                System.Console.Write(a +" " );
+            }
+        }
+        static void quickSort(object rangeObj)
+        {
+            Range range = rangeObj as Range;
+            int[] arr = range.Array;
+            int lower = range.Lower;
+            int upper = range.Upper;
+            if(lower < upper ) //echo entire array |White [blue] white|
+            {
+                int pivot = partition(arr, lower, upper);
+                Thread lowerThread = new Thread(quickSort);
+                Thread upperThread = new Thread(quickSort);
+                lowerThread.Start(new Range { Array = arr, Lower = lower, Upper = pivot - 1 });
+                upperThread.Start( new Range{ Array = arr, Lower = pivot+1, Upper = upper});
+                lowerThread.Join();
+                upperThread.Join();
+            }
+
+        }
+        public static int partition(int[] arr, int lower, int upper)
+        {
+            int pivotIndex = (lower + upper) / 2;
+            int pivotValue = arr[pivotIndex];
+
+            Swap(arr, upper, pivotIndex);
+
+            int Index = lower;
+
+            for (int i = lower; i <= upper - 1; i++)
+            {
+                if (arr[i] < pivotValue)
+                {
+                    Swap(arr, Index, i);
+                    Index++;
+                }
+            }
+
+            Swap(arr, Index, upper);
+
+            return Index;
+        }
+        public static void Swap(int[] ints, int x, int y)
+        {
+            int temp = ints[x];
+            ints[x] = ints[y];
+            ints[y] = temp;
+        }
+    }
+    class Range
+    {
+       public int [] Array{set;get;}
+        public int Upper{set; get;}
+        public int Lower{set;get;} 
+       public int partition(int[] arr, int lower, int upper)
+         {
+          return Class1.partition(arr, lower, upper);
+        }
+
+        /*public void Range{
+            
+        }*/
+    }
+}
+
